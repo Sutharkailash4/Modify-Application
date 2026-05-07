7e3d1802376f59e6c32ec92f660b9d3f4638d0c9
kailash suthar <kailashsuthar4488@gmail.com>
2026-05-07 08:38:11 +0530

    add getme

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -30,12 +30,58 @@ const registerController = async (req, res) => {
                 message : "User Already Exists With This Username or Email"
             })
         }
-        const hash_password = await bcrypt.hash(passowrd, 10);
+        const hash_password = await bcrypt.hash(password, 10);
         const user = await model.create({
             username : username,
             email : email,
             password : hash_password
         })
+
+        const access_token = jwt.sign({
+            id : user._id,
+            email : user.email,
+            username : user.username
+        },
+        process.env.JWT_ACCESS_TOKEN,
+        {
+            expiresIn : "3h"
+        }
+    )
+
+    const refresh_token = jwt.sign({
+        id : user._id,
+        email : user.email,
+        username : user.username
+    },
+    process.env.JWT_REFRESH_TOKEN,
+    {
+        expiresIn : "7d"
+    }
+)
+
+    res.cookie("access_token",access_token,
+        {
+            httpOnly : true,
+            secure : true,
+            sameSite : "strict"
+        }
+    )
+
+    res.cookie("refresh_token",refresh_token,
+        {
+            httpOnly : true,
+            secure : true,
+            sameSite : "strict"
+        }
+    )
+
+    res.status(201).json({
+        message : "User Register Successfully",
+        username : user.username,
+        id : user._id,
+        email : user.email
+    })
+
         }
     } catch(error) {
         res.status(400).json({
@@ -43,12 +89,82 @@ const registerController = async (req, res) => {
             error : error.message
         })
     }
-    console.log("Hello")
 }
 
 const loginController = async (req, res) => {
     try {
+        const data = req.body;
+        if(!data.password || data.password.trim()==="") {
+            return res.status(409).json({
+                message : "Password is Required For Login"
+            })
+        } else if((!data.username || data.username.trim()==="") && (!data.email || data.email.trim()==="")) {
+            return res.status(409).json({
+                message : "Username or Email is Required For Login"
+            })
+        } else {
+            const {username, email, password} = req.body;
+            const isUserExists = await model.findOne({
+                $or : [
+                    {username : username},
+                    {email : email}
+                ]
+            })
+            if(!isUserExists) {
+                return res.status(404).json({
+                    message : "Invalid Credentials"
+            })
+            }
+            const isPasswordCorrect = await bcrypt.compare(password, isUserExists.password);
+            if(!isPasswordCorrect) {
+                return res.status(401).json({
+                    message : "Invalid Credentials"
+                })
+            }
+            const access_token = jwt.sign({
+                id : isUserExists._id,
+                email : isUserExists.email,
+                username : isUserExists.username
+            },
+            process.env.JWT_ACCESS_TOKEN,
+            {
+                expiresIn : "3h"
+            }
+        )
 
+            const refresh_token = jwt.sign({
+                id : isUserExists._id,
+                email : isUserExists.email,
+                username : isUserExists.username
+            },
+            process.env.JWT_REFRESH_TOKEN,
+            {
+                expiresIn : "7d"
+            }
+        )
+
+        res.cookie("access_token",access_token,
+            {
+                httpOnly : true,
+                secure : true,
+                sameSite : "strict"
+            }
+        )
+
+        res.cookie("refresh_token",refresh_token,
+            {
+                httpOnly : true,
+                secure : true,
+                sameSite : "strict"
+            }
+        )
+        res.status(200).json({
+            message : "User Login Successfully",
+            id : isUserExists._id,
+            email : isUserExists.email,
+            username : isUserExists.username
+        })
+        }
     } catch(error) {
         res.status(400).json({
             message : "Something Went Wrong",
@@ -58,8 +174,10 @@ const loginController = async (req, res) => {
 }
 
 const getMeController = async (req, res) => {
-    try {
-
+    try {   
+        const token = req.cookies.access_token;
+        console.log(token);
+        res.send("ok")
     } catch(error) {
         res.status(400).json({
             message : "Something Went Wrong",
