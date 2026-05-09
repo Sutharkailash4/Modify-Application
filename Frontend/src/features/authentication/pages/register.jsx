e04c99b97ec1613c27754da56347f18850b288f4
kailash suthar <kailashsuthar4488@gmail.com>
2026-05-09 13:57:24 +0530

    add css

--- Frontend/src/features/authentication/pages/register.jsx
+++ Frontend/src/features/authentication/pages/register.jsx
@@ -1,17 +1,45 @@
 import React from "react";
+import ".././style/register.css";
 
 const Register = () => {
   return (
     <div className="register-page">
-      <div className="register-heading-box">
-    
-      </div>
-      <div className="register-heading-second-box">
-
-      </div>
-      <div className="register-inputs-box">
-
-      </div>
+      <form>
+        <div className="register-heading-box">
+          <div className="register-heading-img-box">
+            <img src="../../../.././public/face-id.png" alt="image not found" />
+          </div>
+          <div className="register-heading-text-box">
+            <h2>
+              Mood<span className="text-purple-color">ify</span>
+            </h2>
+            <p>Your Mood. Your Music.</p>
+          </div>
+        </div>
+        <div className="register-heading-second-box">
+          <h2>Create Account</h2>
+          <p>Join modify and let your emotion</p>
+          <p>find the perfect song for you.</p>
+        </div>
+        <div className="register-inputs-box">
+          <input type="text" />
+          <input type="email" />
+          <input type="password" />
+          <input type="password" />
+        </div>
+        <div className="terms-box">
+          <input type="checkbox" />
+          <p className="terms-para">
+            I agree to the{" "}
+            <span className="text-purple-color">Terms of Use</span> and{" "}
+            <span className="text-purple-color">Private Policy</span>
+          </p>
+        </div>
+        <button className="register-btn form-btn btn">
+          Create Account
+          <i class="ri-arrow-right-long-fill"></i>
+        </button>
+      </form>
     </div>
   );
 };
