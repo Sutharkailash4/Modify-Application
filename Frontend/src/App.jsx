5464dab8d99b1e6aef64f9646abe21067805116d
kailash suthar <kailashsuthar4488@gmail.com>
2026-05-10 14:05:33 +0530

    chages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import FaceExpression from "./features/expression/components/FaceExpression";
 import { RouterProvider } from "react-router";
 import { router } from "./app.routes";
+import { AuthProvider } from "./features/authentication/auth.context/auth.context";
 
 const App = () => {
   return (
-    <div className="app">
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   );
 };
 
---
+++ Frontend/src/features/authentication/services/auth.api.js
@@ -0,0 +1,50 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL : "http://localhost:3000",
+    withCredentials : true
+});
+
+export const register = async ({username, email, password}) => {
+    try {
+        const response = await api.post("/api/auth/register",
+            {
+                username, email, password
+            }
+        )
+        return response.data
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+export const login = async ({username, email, password}) => {
+    try {
+        const response = await api.post("/api/auth/login",
+            {
+                username, email, password
+            }
+        )
+        return response.data
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+export const getMe = async () => {
+    try {
+        const response = await api.get("/api/auth/getMe");
+        return response.data
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+export const logout = async () => {
+    try {
+        const response = await api.get("/api/auth/logout");
+        return response.data
+    } catch(error) {
+        console.log(error.message);
+    }
+}
