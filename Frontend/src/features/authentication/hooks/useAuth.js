b29aff5c58ddd94b6181bee17c156fc9051b66c6
kailash suthar <kailashsuthar4488@gmail.com>
2026-05-10 17:19:21 +0530

    react 4layer structue

---
+++ Frontend/src/features/authentication/hooks/useAuth.js
@@ -0,0 +1,57 @@
+import { login } from "../services/auth.api";
+import { register } from "../services/auth.api";
+import { getMe } from "../services/auth.api";
+import { logout } from "../services/auth.api";
+import { useContext } from "react";
+import { AuthContext } from "../auth.context/auth.context";
+
+export const useAuth = async () => {
+    const context = useContext(AuthContext);
+    const {user, setUser, loading, setLoading} = context;
+
+    const handleRegister = async ({username, email, password}) => {
+        try {
+            setLoading(true);
+            const data = await register;
+            setUser(data);
+            setLoading(false);
+        } catch(error) {
+            console.log(error.message);
+        }
+    } 
+
+    const handleLogin = async () => {
+        try {
+            setLoading(true);
+            const data = await login;
+            setUser(data)
+            setLoading(false)
+        } catch(error) {
+            console.log(error.message);
+        }
+    }
+
+    const handleGetMe = async () => {
+        try {
+            setLoading(true)
+            const data = await getMe;
+            setUser(data)
+            setLoading(false)
+        } catch(error) {
+            console.log(error.message);
+        }
+    }
+
+    const handleLogout = async () => {
+        try {
+            setLoading(true)
+            const data = await logout;
+            setUser(data)
+            setLoading(false)
+        } catch(error) {    
+            console.log(error.message)
+        }
+    }
+
+    return {handleRegister, handleLogin, handleGetMe, handleLogout};
+}
